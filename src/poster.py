45623dfaccb7b17e2c1517abe16d64a2ddcb3dfa
laufhannes <mail@laufhannes.de>
2017-03-02 08:31:24 +0100

    rename label for number to be not running-specific

--- src/poster.py
+++ src/poster.py
@@ -13,7 +13,7 @@ class Poster:
         self.title = ""
         self.tracks = []
         self.colors = {"background": "#222222", "text": "#FFFFFF", "special": "#FFFF00", "track": "#4DD2FF"}
-        self.statistics = {"label": "Runs", "num": 0, "total": 0.0, "min": 0.0, "max": 0.0}
+        self.statistics = {"label": "Activities", "num": 0, "total": 0.0, "min": 0.0, "max": 0.0}
         self.map_url = ""
         self.map_provider = ""
         self.width = 200
